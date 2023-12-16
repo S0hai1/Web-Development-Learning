1086781f516d2c5721864c59baa3d39071815690
Sohail <78489566+S0hai1@users.noreply.github.com>
2023-12-16 22:28:50 +0530

    Add files via upload

---
+++ Web Development  Learning/Classes/loops-functions-and-callbacks.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+function sqr(n)
+{
+    return n * n;
+
+}
+
+function cube(n)
+{
+    return n*n*n;
+}
+
+function dosme(a,b,fn)
+{
+
+    let v1 = fn(a);
+    let v2 = fn(b);
+
+    return v1+v2;
+
+
+
+}
+//function callback (passing the function name in another function as a parameter)
+console.log(dosme(2,2,cube));
+
+
